419eb09e91d836961e364f6343e62f0aaf8db2d4
MachinEmmus <emonsalvep38@gmail.com>
2020-02-03 14:51:42 -0500

    add selection_ sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+/**
+ * selection_sort - function that sorts an array of integers in ascending or * der using the Selection sort algorithm
+ * @array: is my vector
+ * @size: is the len of my array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, idx, count = 0;
+	int aux, flag = 1;
+	do
+	{
+		flag = 0;
+		aux = array[count];
+		for (i = count + 1; i < size; i++)
+		{
+			if (array[i] < aux)
+			{
+				aux = array[i];
+				idx = i;
+				flag = 1;
+			}
+		}
+		if (flag == 1)
+		{
+			array[idx] = array[count];
+			array[count] = aux;
+			print_array(array, size);
+		}
+		count++;
+	} while (flag || count < size);
+}
