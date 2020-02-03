39684111dcb04e49915a969e5b7b6d59b94a1b8a
MachinEmmus <emonsalvep38@gmail.com>
2020-02-03 18:04:29 -0500

    Improved our code in the task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -61,6 +61,7 @@ void quicksort(int *array, int lo, int hi, size_t size)
   */
 void quick_sort(int *array, size_t size)
 {
-	quicksort(array, 0, size, size);
-
+	if (array == NULL || size < 2)
+		return;
+	quicksort(array, 0, size - 1, size);
 }
