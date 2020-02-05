2d096e1c96a86e23b19dd281f2ea4c030d5d4c6c
MachinEmmus <emonsalvep38@gmail.com>
2020-02-05 11:07:40 -0500

    Revert "creating our owner logic to task four"
    This reverts commit 21f935689684c22ee01359847c143f91c2d23b82.

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -7,44 +7,24 @@
   */
 void shell_sort(int *array, size_t size)
 {
-int cont2, cont3, tmp, k = 0, cont = 0, flag = 1;
+	int j, i, tmp, k = 0;
+
 	if (array == NULL || size < 2)
 		return;
 	while (k * 3 + 1 <= (int)size)
+	{
 		k = k * 3 + 1;
+	}
 	while (k > 0)
 	{
-		while ((cont + k) < (int)size)
+		for (i = k; i < (int)size; i++)
 		{
-			cont2 = cont;
-			while ((cont2 + k) < (int)size)
-			{
-				if (array[cont2] > array[cont2 + k])
-				{
-					tmp = array[cont2];
-					array[cont2] = array[cont2 + k];
-					array[cont2 + k] = tmp;
-					cont3 = cont2;
-					flag = 1;
-					while ((cont3 - k) > -1 && flag)
-					{
-						if (array[cont3 - k] > array[cont3])
-						{
-							tmp = array[cont3];
-							array[cont3] = array[cont3 - k];
-							array[cont3 - k] = tmp;
-						}
-						else
-							flag = 0;
-						cont3 -= k;
-					}
-				}
-				cont2 += k;
-			}
-			cont++;
+			tmp = array[i];
+			for (j = i; j >= k && array[j - k] > tmp; j -= k)
+				array[j] = array[j - k];
+			array[j] = tmp;
 		}
 		print_array(array, size);
 		k = (k - 1) / 3;
 	}
 }
-
